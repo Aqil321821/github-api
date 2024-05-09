a08473dceef6279c74b4d9b73b2bc1988a73e5bd
Aqil321821 <aqilrao321821@gmail.com>
2024-05-09 23:30:43 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const xhr = new XMLHttpRequest();
+
+
+xhr.open('GET', 'https://api.github.com/users/aqil321821/repos');
+
+
+xhr.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    // console.log(JSON.parse(this.responseText));
+    const data = JSON.parse(this.responseText);
+    // console.log(data);
+
+    data.forEach((repo) => {
+      const li = document.createElement('li');
+      li.innerHTML = `<strong>${repo.name}</strong> - ${repo.description}`;
+      document.querySelector('#results').appendChild(li);
+    });
+  }
+};
+
+// Send request
+xhr.send();
+
+
+////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
